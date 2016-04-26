b435eb1c30d6e998400995606294bdafacfa6dbb
mohaiminul-sust <mohaiminul.sust@csesociety.org>
2016-04-26 06:44:18 +0600

    RenameUtil Built

--- src/bru/RenameUtil.java
+++ src/bru/RenameUtil.java
@@ -7,55 +7,62 @@ package bru;
 
 import java.io.File;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  *
  * @author Andromeda
  */
 public class RenameUtil {
-    
-    private String path = "";
+
+    private List<String> paths;
     private final HashMap<String, String> map = new HashMap<>();
-    
-    public RenameUtil(String path){
-        this.path = path;
+
+    public RenameUtil(List<String> paths) {
+        this.paths = paths;
     }
-    
-    public void IndexedRenaming(String name){
-        System.out.println("Selected Path for indexed renaming: "+path);
-        
-        File dir = new File(path);
 
-        if (dir.exists()) {
+    public void IndexedRenaming(int starts_from, String name) {
 
-            if (dir.isDirectory()) {
-                File[] files = dir.listFiles();
-
-                int i = 0;
-                for (File file : files) {
-                    String oldName = file.getName();
-                    System.out.println("Old Name: " + oldName);
-                    String extension = getExtension(oldName);
-                    String newName = name + "_" + i + extension;
-                    String newPath = path + "\\" + newName;
-                    file.renameTo(new File(newPath));
-                    System.out.println(oldName + " changed to " + newName);
-                    i++;
-                }
-            } else if(dir.isFile()){
-                System.out.println("indexed renaming supported for single file");
-            }
+        Iterator iter = paths.iterator();
+        while (iter.hasNext()) {
             
-            System.out.println("task finished !");
+            String path = iter.next().toString();
+            System.out.println("Selected Path for indexed renaming: " + path);
 
+            File dir = new File(path);
+
+            if (dir.exists()) {
+
+                if (dir.isDirectory()) {
+                    File[] files = dir.listFiles();
+
+                    int i = starts_from;
+                    for (File file : files) {
+                        String oldName = file.getName();
+                        System.out.println("Old Name: " + oldName);
+                        String extension = getExtension(oldName);
+                        String newName = name + "_" + i + extension;
+                        String newPath = path + "\\" + newName;
+                        file.renameTo(new File(newPath));
+                        System.out.println(oldName + " changed to " + newName);
+                        i++;
+                    }
+                } else if (dir.isFile()) {
+                    System.out.println("indexed renaming supported for single file");
+                }
+
+                System.out.println("task finished !");
+
+            }
         }
     }
-    
-    
-    private static String getExtension(String fileName){
-        
+
+    private static String getExtension(String fileName) {
+
         try {
-            return "."+fileName.substring(fileName.lastIndexOf(".") + 1);
+            return "." + fileName.substring(fileName.lastIndexOf(".") + 1);
         } catch (Exception ex) {
             return "";
         }
